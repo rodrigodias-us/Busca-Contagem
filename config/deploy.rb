9fd644d79e1c19df41901bfcf23bab904b694ded
Rodrigo Dias <contato@rodrigodias.us>
2013-02-16 16:13:33 -0200

    Deploy My Server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 set :application, "Busca Contagem"
-set :repository,  "git@github.com:rodrigodias-us/Busca-Contagem.git"
+set :repository,  "git://github.com/rodrigodias-us/Busca-Contagem.git"
 
 set :deploy_to, "/home/buscacontagem/public_html"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "www.buscacontagem.com.br"                          # Your HTTP server, Apache/etc
-role :app, "www.buscacontagem.com.br"                          # This may be the same as your `Web` server
+role :web, "buscacontagem.com.br"                          # Your HTTP server, Apache/etc
+role :app, "buscacontagem.com.br"                          # This may be the same as your `Web` server
 role :db,  "buscacontagem.com.br", :primary => true # This is where Rails migrations will run
 role :db,  "buscacontagem.com.br"
 
